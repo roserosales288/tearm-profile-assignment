3d761812ddd36889694003086764507f1d3c47e5
Diamond Rosales <roserosales288@gmail.com>
2021-08-30 12:40:12 -0400

    test failed

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,10 +1,10 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../lib/Employee");
+
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+  expect(officeNumber).toBe(testValue);
 });
 
 test("getRole() should return \"Manager\"", () => {
